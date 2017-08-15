5f4551a0257bad9dec00238bc4fb08bc05d57f12
Scott Taylor <scott.c.taylor@mac.com>
2017-08-15 16:17:32 -0400

    export App wrapper

--- src/App/index.js
+++ src/App/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'emotion/react';
-import { ThemeProvider as ThemingProvider, withTheme } from 'theming';
+import { ThemeProvider, withTheme } from 'theming';
 import theme from '../theme';
 import responsive from '../responsive';
 import Header from '../Header';
 import Sidebar from '../Sidebar';
+import '../global';
 
 export { Header, Sidebar };
-
-export const ThemeProvider = ({ children }) =>
-  <ThemingProvider theme={theme}>
-    {children}
-  </ThemingProvider>;
-
-ThemeProvider.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export const PageWrapper = withTheme(styled.div`
   background: ${p => p.theme.colors.white};
@@ -55,3 +47,30 @@ export const Footer = () =>
     <a href="https://twitter.com/wonderboymusic">@wonderboymusic</a>&nbsp; ... Powered by GraphQL /
     React / GraphQL / WordPress / Emotion
   </StyledFooter>;
+
+export default function App({ settings, navMenu, sidebar, children }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <PageWrapper>
+        <Header {...{ settings, navMenu }} />
+        <Content>
+          <Primary>
+            {children}
+          </Primary>
+          <Secondary>
+            <Sidebar sidebar={sidebar} />
+          </Secondary>
+        </Content>
+        <Footer />
+      </PageWrapper>
+    </ThemeProvider>
+  );
+}
+
+App.propTypes = {
+  /* eslint-disable react/forbid-prop-types */
+  settings: PropTypes.object.isRequired,
+  navMenu: PropTypes.object.isRequired,
+  sidebar: PropTypes.object.isRequired,
+  children: PropTypes.node.isRequired,
+};
