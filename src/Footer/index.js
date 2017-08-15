47610cc13269e9cc64d952ddf5b8b48c870947d7
Scott Taylor <scott.c.taylor@mac.com>
2017-08-15 14:15:17 -0400

    export footer and image

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,9 +1,18 @@
+import React from 'react';
 import styled from 'emotion/react';
 import { withTheme } from 'theming';
 
-const Footer = withTheme(styled.footer`
+const StyledFooter = withTheme(styled.footer`
   padding: ${p => p.theme.padding}px 0;
   text-align: center;
 `);
 
-export default Footer;
+export default function Footer() {
+  return (
+    <StyledFooter>
+      © Scott Taylor ...&nbsp; Brooklyn, NY ...{' '}
+      <a href="https://twitter.com/wonderboymusic">@wonderboymusic</a>&nbsp; ... Powered by GraphQL
+      / React / GraphQL / WordPress / Emotion
+    </StyledFooter>
+  );
+}
