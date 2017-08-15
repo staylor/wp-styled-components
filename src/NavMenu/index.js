af994c8692da2e01afa057a817e71aeafc22fed1
Scott Taylor <scott.c.taylor@mac.com>
2017-08-15 15:33:46 -0400

    export NavMenu

--- src/NavMenu/index.js
+++ src/NavMenu/index.js
@@ -1,6 +1,11 @@
+import url from 'url';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'emotion/react';
 import { withTheme } from 'theming';
+import { Link } from 'found';
 import responsive from '../responsive';
+import { sortOrderedHierarchy } from '../walker';
 
 export const NavWrapper = withTheme(styled.nav`
   display: block;
@@ -71,3 +76,85 @@ export const Level = withTheme(styled.ul`
 `);
 
 export const NavItem = styled.li`display: inline-block;`;
+
+export default class NavMenu extends Component {
+  static propTypes = {
+    navMenu: PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      items: PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.string,
+          title: PropTypes.string,
+          url: PropTypes.string,
+          parent: PropTypes.string,
+          order: PropTypes.number,
+          type: PropTypes.string,
+          typeName: PropTypes.string,
+          typeSlug: PropTypes.string,
+          dataSlug: PropTypes.string,
+          dataID: PropTypes.string,
+        })
+      ),
+    }).isRequired,
+  };
+
+  sorted = null;
+  level = 0;
+
+  parseItem({ id, title, url: itemUrl, typeSlug, dataSlug }) {
+    let path;
+    if (typeSlug && dataSlug) {
+      path = `/${typeSlug}/${dataSlug}`;
+    } else {
+      const urlObj = url.parse(itemUrl);
+      if (urlObj.path === '/') {
+        path = urlObj.path;
+      } else {
+        path = urlObj.path.replace(/\/$/, '');
+      }
+    }
+
+    if (this.sorted[id]) {
+      this.level += 1;
+    }
+    return (
+      <NavItem key={id}>
+        <Link to={path}>
+          {title}
+        </Link>
+        {this.sorted[id] ? this.walk(this.sorted[id]) : null}
+      </NavItem>
+    );
+  }
+
+  walk(node) {
+    return (
+      <Level>
+        {node.map(child => {
+          if (!child.parent) {
+            this.level = 0;
+          }
+          return this.parseItem(child);
+        })}
+      </Level>
+    );
+  }
+
+  render() {
+    const { navMenu } = this.props;
+
+    if (!navMenu) {
+      return null;
+    }
+
+    this.sorted = sortOrderedHierarchy(navMenu.items);
+    const navMenuHtml = this.walk(this.sorted.top);
+
+    return (
+      <NavWrapper>
+        {navMenuHtml}
+      </NavWrapper>
+    );
+  }
+}
