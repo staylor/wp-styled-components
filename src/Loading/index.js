bff8b34fd397e3017a491b37894e1cd74b0bb72d
Scott Taylor <scott.c.taylor@mac.com>
2017-08-29 14:43:23 -0400

    upgrade deps

--- src/Loading/index.js
+++ src/Loading/index.js
@@ -9,7 +9,7 @@ export const Container = withTheme(styled.div`
 `);
 
 export const ActivityIndicator = withTheme(styled.div`
-  background: ${p => p.theme.colors.white} url('/images/icons/icon-activity-indicator.gif')
+  background: ${p => p.theme.colors.white} url("/images/icons/icon-activity-indicator.gif")
     no-repeat 50% 10px;
   height: 56px;
   width: 100%;
