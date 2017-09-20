72a6c61440acb3d934ff42512241c03f04c435ca
Scott Taylor <scott.c.taylor@mac.com>
2017-09-20 18:49:49 -0400

    CSS activity

--- src/Loading/index.js
+++ src/Loading/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { css, keyframes } from 'emotion';
 import styled from 'react-emotion';
 import { withTheme } from 'theming';
+import theme from '../theme';
 
 export const Container = withTheme(styled.div`
   background: ${p => p.theme.colors.white};
@@ -8,17 +10,46 @@ export const Container = withTheme(styled.div`
   padding: 200px 0 0;
 `);
 
-export const ActivityIndicator = withTheme(styled.div`
-  background: ${p => p.theme.colors.white} url('/images/icons/icon-activity-indicator.gif')
-    no-repeat 50% 10px;
-  height: 56px;
+const Wrap = styled.div`
+  height: 40px;
+  position: relative;
+  width: 40px;
+`;
+
+const bounce = keyframes`
+  0%, 100% {
+    transform: scale(0.0);
+  }
+  50% {
+    transform: scale(1.0);
+  }
+`;
+
+const bouncing = css`
   width: 100%;
-`);
+  height: 100%;
+  border-radius: 50%;
+  background-color: ${theme.colors.pink};
+  opacity: 0.6;
+  position: absolute;
+  top: 0;
+  left: 0;
+  animation: ${bounce} 2s infinite ease-in-out;
+`;
+
+const Bounce1 = styled.div`composes: ${bouncing};`;
+const Bounce2 = styled.div`
+  composes: ${bouncing};
+  animation-delay: -1s;
+`;
 
 export default function Loading() {
   return (
     <Container>
-      <ActivityIndicator />
+      <Wrap>
+        <Bounce1 />
+        <Bounce2 />
+      </Wrap>
     </Container>
   );
 }
